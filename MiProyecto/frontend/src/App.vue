fd004199daedf056d80ebff033d9b4a335b7be8c
A24Ole <a24oleproyat@inspedralbes.cat>
2025-10-21 11:55:47 +0200

    Separació entre back y front.

---
+++ MiProyecto/frontend/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref, computed} from 'vue';
+</script>
+
+<template>
+  <header>
+    <h1>Mi Aplicación Vue</h1>
+  </header>
+  <main>
+    <p>¡Bienvenido a mi aplicación Vue!</p>
+  </main>
+  <footer>
+    <p>&copy; 2023 Mi Aplicación Vue</p>
+  </footer>
+</template>
+
+<style scoped>
+
+</style>
